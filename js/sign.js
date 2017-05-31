5de72f417b00c9646995f28141608704bc680df8
Lively Magic Architect <1757977307@qq.com>
2017-05-31 17:35:03 +0800

    update

--- js/sign.js
+++ js/sign.js
@@ -1,7 +1,6 @@
  $(function () {
-    $("#register-form").bootstrapValidator({
+    $("#register-form-signup").bootstrapValidator({
         submitHandler: function (valiadtor, loginForm, submitButton) {
-
             valiadtor.defaultSubmit();
         },
         fields: {
@@ -61,7 +60,62 @@
                     }
                 }
             }
-
+        }
+    });
+    $("#register-form-signin").bootstrapValidator({
+        submitHandler: function (valiadtor, loginForm, submitButton) {
+            valiadtor.defaultSubmit();
+        },
+        fields: {
+            userName: {
+                validators: {
+                    notEmpty: {
+                        message: '用户名不能为空'
+                    },
+                    regexp:{
+                        regexp:/^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(17[013678])|(18[0-9]))\d{8}$/,
+                        message:"请输入合法的手机号"
+                    }
+                }
+            },
+            password: {
+                validators: {
+                    notEmpty: {
+                        message: '密码不能为空'
+                    },
+                    stringLength: {
+                        min: 6,
+                        max: 30,
+                        message: '密码长度必须在6到30之间'
+                    },
+                    different: {
+                        field: 'loginName',
+                        message: '不能和用户名相同'
+                    },
+                    regexp: {
+                        regexp: /^[a-zA-Z0-9_\.]+$/,
+                        message: '密码由数字字母下划线和.组成'
+                    }
+                }
+            }
+        }
+    });
+    $("#register-form-signforget").bootstrapValidator({
+        submitHandler: function (valiadtor, loginForm, submitButton) {
+            valiadtor.defaultSubmit();
+        },
+        fields: {
+            userName: {
+                validators: {
+                    notEmpty: {
+                        message: '用户名不能为空'
+                    },
+                    regexp:{
+                        regexp:/^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(17[013678])|(18[0-9]))\d{8}$/,
+                        message:"请输入合法的手机号"
+                    }
+                }
+            }
         }
     });
 });
